bfa5f798729156357641f7858bf2ec8405dc43cd
Brynfor Morris <brynfor.morris@gmail.com>
2023-02-16 17:29:48 -0600

    Possible Solution for Palindrome Problem

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  arrayOfCharacters = word.split("");
+  // console.log(arrayOfCharacters);
+  let testingArray = [];
+
+  for (let i = 0; i < arrayOfCharacters.length; i++){
+    let value;
+    if(arrayOfCharacters[i] === arrayOfCharacters[arrayOfCharacters.length-i-1]){
+      value = true;
+    } else {
+      value = false;
+    }
+
+    testingArray.push(value)
+
+    };
+  
+    if (testingArray.find( arrayElement => arrayElement === false ) === undefined) {
+      return true;
+    } else {
+      return false;
+    };
+  }
 
 /* 
-  Add your pseudocode here
+  Function takes one string as argument
+  returns true IF:
+  when iterating through the word, the first and last position are equal and those therin
+  character in first position === character in last position
+  character in second position === character in second to last position.
+  etc..
+   
+  else false
 */
 
 /*
@@ -12,13 +39,20 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
+  console.log("Expecting: true //", "Word: refer");
+  console.log("=>", isPalindrome("refer"));
+
+  console.log("Expecting: false //", "Word: duckos");
+  console.log("=>", isPalindrome('duckos'));
+
   // add your own custom tests in here
-  console.log("Expecting: true");
+  console.log("Expecting: true //", "Word: racecar");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true //", "Word: saippuakivikauppias");
+  console.log("=>", isPalindrome("saippuakivikauppias"));
 
-  console.log("Expecting: false");
+  console.log("Expecting: false //", "Word: robot");
   console.log("=>", isPalindrome("robot"));
 }
 
